ff11929b1ff41282848fbe2bd538f8dec0744e9c
brover <95491506+broverz@users.noreply.github.com>
2022-11-09 20:45:45 +0700

    jk

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import random
+
+if random.randint(0, 11) == 1:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('Hello World!!')
+else:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('TT')
